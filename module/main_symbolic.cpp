1dee7a1b4a06ced2e0ec41a87e7756b2ba697a54
metrovian <paramedoct@gmail.com>
2025-07-07 21:35:39 +0900

    멀티플랫폼 빌드 스크립트 생성

---
+++ module/main_symbolic.cpp
@@ -0,0 +1,11 @@
+#include <Eigen/Core>
+
+int main() {
+	printf(
+	    "Eigen version: %d.%d.%d\n",
+	    EIGEN_WORLD_VERSION,
+	    EIGEN_MAJOR_VERSION,
+	    EIGEN_MINOR_VERSION);
+
+	return 0;
+}
