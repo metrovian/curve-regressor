84ba206dc7d6dd64204402ea2dcd7e3df2053913
metrovian <paramedoct@gmail.com>
2025-07-08 22:20:17 +0900

    표현식 라이브러리 서브모듈 업로드

--- module/main_classic.cpp
+++ module/main_classic.cpp
@@ -1,11 +1,25 @@
 #include <Eigen/Core>
 
+extern "C" {
+#include "tinyexpr.h"
+}
+
 int main() {
 	printf(
 	    "Eigen version: %d.%d.%d\n",
 	    EIGEN_WORLD_VERSION,
 	    EIGEN_MAJOR_VERSION,
 	    EIGEN_MINOR_VERSION);
+
+	const char *expr_str = "sqrt(2)";
+	te_expr *expr = te_compile(expr_str, NULL, 0, NULL);
+	if (expr) {
+		double result = te_eval(expr);
+		printf("Result: %f\n", result);
+		te_free(expr);
+	} else {
+		printf("Failed to compile expression.\n");
+	}
 
 	return 0;
 }
