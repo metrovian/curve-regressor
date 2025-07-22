1d1f67b0c98c6edc4a92efee525d1fface0c6d03
metrovian <paramedoct@gmail.com>
2025-07-22 22:21:28 +0900

    데이터 시뮬레이터 분리

--- source/sensor_simulator.c
+++ source/sensor_simulator.c
@@ -5,22 +5,30 @@ static double sensor_simulator_random(double minimum, double maximum) {
 	return random * (maximum - minimum) + minimum;
 }
 
+static double sensor_simulator_rtd(double domain) {
+	const static double CONST_RTD_R0 = 1.0000E+02;
+	const static double CONST_RTD_A = 3.9083E-03;
+	const static double CONST_RTD_B = -5.7750E-07;
+	return CONST_RTD_R0 * (1.0000E+00 + CONST_RTD_A * domain + CONST_RTD_B * domain * domain);
+}
+
+static double sensor_simulator_pd(double domain) {
+	const static double CONST_PD_P0 = 1.0000E+01;
+	const static double CONST_PD_A = 1.0000E-03;
+	return CONST_PD_P0 / domain / domain + CONST_PD_A;
+}
+
 extern int8_t sensor_simulator(sensor_simulator_t sensor, int32_t counts, double *domain, double *range) {
 	for (int32_t i = 0; i < counts; ++i) {
 		switch (sensor) {
 		case SENSOR_RTD: {
-			const static double CONST_RTD_R0 = 1.0000E+02;
-			const static double CONST_RTD_A = 3.9083E-03;
-			const static double CONST_RTD_B = -5.7750E-07;
 			domain[i] = sensor_simulator_random(0, 650);
-			range[i] = CONST_RTD_R0 * (1 + CONST_RTD_A * domain[i] + CONST_RTD_B * domain[i] * domain[i]);
+			range[i] = sensor_simulator_rtd(domain[i]);
 		}
 
 		case SENSOR_PD: {
-			const static double CONST_PD_P0 = 1.0000E+01;
-			const static double CONST_PD_A = 1.0000E-04;
 			domain[i] = sensor_simulator_random(1.0, 5.0);
-			range[i] = CONST_PD_P0 * CONST_PD_A / domain[i] / domain[i];
+			range[i] = sensor_simulator_pd(domain[i]);
 		}
 
 		default: {
@@ -30,3 +38,4 @@ extern int8_t sensor_simulator(sensor_simulator_t sensor, int32_t counts, double
 
 		return 0;
 	}
+}
