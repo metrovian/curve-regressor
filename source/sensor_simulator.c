fc5dd6af080fdfa66e546cf3aea1300261c34b1c
metrovian <paramedoct@gmail.com>
2025-07-23 21:07:37 +0900

    LVDT 데이터 시뮬레이터 구현

--- source/sensor_simulator.c
+++ source/sensor_simulator.c
@@ -9,7 +9,8 @@ static double sensor_simulator_rtd(double domain) {
 	const static double CONST_RTD_R0 = 1.0000E+02;
 	const static double CONST_RTD_A = 3.9083E-03;
 	const static double CONST_RTD_B = -5.7750E-07;
-	return CONST_RTD_R0 * (1.0000E+00 + CONST_RTD_A * domain + CONST_RTD_B * domain * domain);
+	const static double CONST_RTD_X = 1.0000E+00;
+	return CONST_RTD_R0 * (CONST_RTD_X + CONST_RTD_A * domain + CONST_RTD_B * domain * domain);
 }
 
 static double sensor_simulator_pd(double domain) {
@@ -18,17 +19,31 @@ static double sensor_simulator_pd(double domain) {
 	return CONST_PD_P0 / domain / domain + CONST_PD_A;
 }
 
+static double sensor_simulator_lvdt(double domain) {
+	const static double CONST_LVDT_V0 = 2.0000E-02;
+	const static double CONST_LVDT_V1 = 1.0000E-04;
+	return CONST_LVDT_V0 * domain + CONST_LVDT_V1 * domain * domain * domain;
+}
+
 extern int8_t sensor_simulator(sensor_simulator_t sensor, int32_t counts, double *domain, double *range) {
 	for (int32_t i = 0; i < counts; ++i) {
 		switch (sensor) {
 		case SENSOR_RTD: {
-			domain[i] = sensor_simulator_random(0, 650);
+			domain[i] = sensor_simulator_random(0.0000E+00, 6.5000E+02);
 			range[i] = sensor_simulator_rtd(domain[i]);
+			break;
 		}
 
 		case SENSOR_PD: {
-			domain[i] = sensor_simulator_random(1.0, 5.0);
+			domain[i] = sensor_simulator_random(1.0000E+00, 5.0000E+00);
 			range[i] = sensor_simulator_pd(domain[i]);
+			break;
+		}
+
+		case SENSOR_LVDT: {
+			domain[i] = sensor_simulator_random(-1.5000E+01, 1.5000E+01);
+			range[i] = sensor_simulator_lvdt(domain[i]);
+			break;
 		}
 
 		default: {
